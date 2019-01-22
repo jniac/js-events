380da6f2b1c264fe5494579462e992a67ae3322c
jniac <joseph.merdrignac@gmail.com>
2019-01-22 18:17:52 +0100

    init

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+
+export default {
+
+    input: 'src/events.js',
+
+    output: {
+        format: 'iife',
+        name: 'events',
+        file: 'build/events.js',
+        indent: '\t',
+    }
+
+}
